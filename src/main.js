76ac2181a0cb8138281d09d16b9f6b34ed2e8200
Abduaziz-Codecrafter <abduaziz.daily@gmail.com>
2024-12-15 02:22:16 +0300

    The game scene with ramping speed of +50 for now.

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import k from "./kaplayCtx";
+import game from "./scenes/game";
 import mainMenu from "./scenes/mainMenu";
 
 k.loadSprite("chemical-bg", "graphics/chemical-bg.png");
@@ -35,7 +36,7 @@ k.loadSound("city", "sounds/City.wav");
 
 k.scene("main-menu", mainMenu);
 
-k.scene("game", () => {});
+k.scene("game", game);
 
 k.scene("gameover", () => {});
 
